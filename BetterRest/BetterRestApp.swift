6f4b27bf2b77e598fd4fc1cf6593009c8f8f66de
Brian Tavares <btavares79@gmail.com>
2023-06-02 08:07:48 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Brian Tavares on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
